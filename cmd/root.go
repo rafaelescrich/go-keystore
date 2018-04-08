c086259120d8deb189c653ce7a68717152e23b12
Rafael Escrich <rafaelescrich@gmail.com>
2018-04-08 19:15:15 -0300

    Adding database

--- cmd/root.go
+++ cmd/root.go
@@ -2,7 +2,6 @@ package cmd
 
 import (
 	"fmt"
-	"os"
 
 	"github.com/spf13/cobra"
 )
@@ -19,6 +18,6 @@ var rootCmd = &cobra.Command{
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
-		os.Exit(1)
+		// os.Exit(1)
 	}
 }
--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"github.com/rafaelescrich/go-keystore/cmd"
+	"github.com/rafaelescrich/go-keystore/database"
 )
 
 func main() {
 	cmd.Execute()
+
+	database.InitDB()
 }
