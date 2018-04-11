f4208f482781dfd25e98e61dcfb5da1cf10267f1
Rafael Escrich <rafaelescrich@gmail.com>
2018-04-11 19:49:03 -0300

    Added insert and delete from db

--- database/db.go
+++ database/db.go
@@ -2,18 +2,52 @@ package database
 
 import (
 	"fmt"
-	"log"
+	"os"
 
 	"github.com/boltdb/bolt"
 )
 
+// BoltDB pointer
+type BoltDB struct {
+	DB *bolt.DB
+}
+
 // InitDB opens or create db file
-func InitDB() {
+func InitDB() *BoltDB {
 
 	db, err := bolt.Open("keystore.db", 0600, nil)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Printf("BoltDB Error: %s \r\n", err)
+		os.Exit(1)
 	}
-	fmt.Println("DB open")
-	defer db.Close()
+
+	return &BoltDB{db}
+}
+
+// Insert saves on satabase a key value pair
+func (db BoltDB) Insert(key string, value string) error {
+	err := db.DB.Update(func(tx *bolt.Tx) error {
+		b, err := tx.CreateBucketIfNotExists([]byte("keyBucket"))
+		if err != nil {
+			return err
+		}
+		err = b.Put([]byte(key), []byte(value))
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Delete the key from database
+func (db BoltDB) Delete(key string) error {
+	return db.DB.Update(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket([]byte("keyBucket"))
+		return bucket.Delete([]byte(key))
+	})
 }
---
+++ keystore/keystore.go
@@ -0,0 +1,7 @@
+package keystore
+
+// Keystore holds the file name and its key
+type Keystore struct {
+	Filename string
+	Key      string
+}
